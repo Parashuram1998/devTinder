887b4ef9fd8864815eef5139063874a1644e376a
Parasharam Pujari <pujarip516@gmail.com>
2024-12-15 16:15:02 +0530

    Exploring the HTTP methods (GET, POST & DELETE)

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send("Namaste from home page");
+app.get('/user',(req,res) => {
+    res.send({firstname : 'Parasharam', lastname : 'Pujari'});
 });
 
-app.get('/test', (req, res) => {
-    res.send("Namaste from test page");
-});
+app.post("/user", (req, res) => {
+    res.send("User added successfully");
+})
 
-app.get('/hello', (req, res) => {
-    res.send("Namaste from Hello page");
-});
+app.delete('/user', (req,res) => {
+    res.send("User deleted successfully");
+})
 
 app.listen(7777, () => {
     console.log("Server is successfully running on port 7777");
