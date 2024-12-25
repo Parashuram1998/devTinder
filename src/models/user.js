e4ef70d69c05ca391f400877fa15b8faa018912a
Parasharam Pujari <pujarip516@gmail.com>
2024-12-25 18:27:10 +0530

    User Schema Validations

--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,49 @@ const mongoose = require("mongoose");
 //Creating the userSchema
 const userSchema = new mongoose.Schema({
     firstName : {
-        type: String
+        type: String,
+        required : true,
+        minLength : 3,
+        maxLength : 20
     },
     lastName : {
         type: String
     },
     email : {
-        type: String
+        type: String,
+        required : true,
+        trim : true,
+        lowercase : true,
+        unique : true
     },
     age : {
-        type : Number
+        type : Number,
+        min : 18
     },
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["Male","Female","Other"]){
+                throw new error("Gender data is not valid");
+            }
+        }
     },
     password : {
         type : String
+    },
+    photoUrl : {
+       type : String,
+       default : "https://cdn-icons-png.flaticon.com/512/9187/9187604.png" 
+    },
+    about : {
+        type : String,
+        default : "I am a developer!!"
+    },
+    skills : {
+        type : [String]
     }
+},{
+    timestamps : true
 })
 
 //Creating & Exporting the user model.
