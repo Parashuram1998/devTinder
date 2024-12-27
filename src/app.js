922edd43f605cfeb85ae0e98b0350051bba43dbc
Parasharam Pujari <pujarip516@gmail.com>
2024-12-27 22:09:23 +0530

    Authentication, JWT & Cookies

--- src/app.js
+++ src/app.js
@@ -2,157 +2,104 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
-const {validateSignUpData} = require("./utils/validations");
+const { validateSignUpData } = require("./utils/validations");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { userAuth } = require("./middlewares/auth");
 
 //using express.json() we will be able to get all the json data present in request body converted into JS object
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
-    try{
-        //Validating the data
-        validateSignUpData(req); 
+  try {
+    //Validating the data
+    validateSignUpData(req);
 
-        //encrypting the password
-        const {firstName, lastName, email, password} = req.body; 
-        
-        const passwordHash = await bcrypt.hash(password, 10);
+    //encrypting the password
+    const { firstName, lastName, email, password } = req.body;
 
-        //Creating a new instance of the user model
-        const user = new User({
-            firstName, 
-            lastName,
-            email,
-            password : passwordHash
-        })
-    
-        await user.save();
-        res.send("User Data added Successfully");
-    }
-    catch(err) {
-        res.status(400).send("ERROR : "+ err.message)
-    }
-    
-})
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    //Creating a new instance of the user model
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User Data added Successfully");
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
 
 //Login
 app.post("/login", async (req, res) => {
-    try{
-        const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if(validator.isEmail(email)){
-            const user = await User.findOne({email : email});
-            
-            if(!user){
-                throw new Error("Invalid Credentials");
-            }
+    if (validator.isEmail(email)) {
+      const user = await User.findOne({ email: email });
 
-            //Comparing the password entered by user and passwordhash present in db
-            const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!user) {
+        throw new Error("Invalid Credentials");
+      }
 
-            if(isPasswordValid){
-                res.send("Login Successful");
-            }else{
-                throw new Error("Invalid Credentials");
-            }
-        }
-        else{
-            throw new Error("Email is not valid");
-        }
+      //Comparing the password entered by user and passwordhash present in db
+      const isPasswordValid = await user.validatePassword(password);
+
+      if (isPasswordValid) {
+        const token = await user.getJWT();
+        //the cookie will be expired after 1 day.
+        res.cookie("token", token, {
+          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        });
+        res.send("Login Successful!!!!");
+      } else {
+        throw new Error("Invalid Credentials");
+      }
+    } else {
+      throw new Error("Email is not valid");
     }
-    catch(err){
-        res.status(404).send("ERROR : "+ err.message);
-    }
-})
+  } catch (err) {
+    res.status(404).send("ERROR : " + err.message);
+  }
+});
 
-//Get user by email
-app.get("/userByEmail", async (req, res) => {
-    const userEmail = req.body.email;
+//Get user profile
+app.get("/profile", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong!! " + err.message);
+  }
+});
 
-    try{
-        const user = await User.findOne({email : userEmail});
-        
-        if(!user){
-            res.status(404).send("User not found");
-        }else{
-            res.send(user);
-        }        
-    }
-    catch(err){
-        res.status(400).send("Something went wrong!! " + err.message);
-    }
-})
-
-//Get all the users for Feed page
-app.get("/feed", async (req, res) => {
-    try{
-        const users = await User.find({});
-
-        if(users.length === 0){
-            res.status(404).send("Users not found");
-        }else{
-            res.send(users);
-        }        
-    }
-    catch(err){
-        res.status(400).send("Something went wrong!! " + err.message);
-    }
-})
-
-//Delete the user
-app.delete("/user", async (req, res) => {
-    const userId = req.body.userId;
-    try{
-        const deletedUser = await User.findByIdAndDelete(userId);
-        if(deletedUser != 0){
-            res.send("Deleted the user successfully..");
-        }else{
-            res.send("User not found..");
-        }        
-    }
-    catch(err){
-        res.status(400).send("Something went wrong!! " + err.message);
-    }
-})
-
-//Update the user
-app.patch("/user/:userId", async (req,res) => {
-    const userId = req.params?.userId;
-    const data = req.body;
-
-    try{
-            const AllowedUpdates = ["gender","password","about","skills","photoUrl"];
-            const isUpdateAllowed = Object.keys(data).every((k) => AllowedUpdates.includes(k));
-
-            if(!isUpdateAllowed){
-                throw new Error("Update not allowed");
-            }
-            
-            if(!data.skills === undefined){
-                if(data?.skills.length > 10){
-                    throw new Error("Skills cannot be more than 10");
-                }
-            }            
-
-        await User.findByIdAndUpdate({_id: userId}, data, {returnDocument : "after", runValidators : true});
-        res.send("User updated successfully!!");
-    }catch(err){
-        res.status(400).send("Something went wrong!! " + err.message);
-    }
-})
+//sendConnectionRequest
+app.post("/sendConnectionRequest", userAuth, (req, res) => {
+  try {
+    const user = req.user;
+    console.log("Connection request is sent!!");
+    res.send("Connection request is sent by " + user.firstName);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
 
 connectDB()
-    .then(() => {
-        console.log("Database connection established");
+  .then(() => {
+    console.log("Database connection established");
 
-        app.listen(7777, () => {
-            console.log("Server is successfully running on port 7777");
-        });
-
-    })
-    .catch((err)=> {
-        console.error("Database is not connected");
+    app.listen(7777, () => {
+      console.log("Server is successfully running on port 7777");
     });
-
-
+  })
+  .catch((err) => {
+    console.error("Database is not connected");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,69 +1,94 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Creating the userSchema
-const userSchema = new mongoose.Schema({
-    firstName : {
-        type: String,
-        required : true,
-        minLength : 3,
-        maxLength : 20
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
     },
-    lastName : {
-        type: String
+    lastName: {
+      type: String,
     },
-    email : {
-        type: String,
-        required : true,
-        trim : true,
-        lowercase : true,
-        unique : true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Invalid Email Address : "+ value);
-            }
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Address : " + value);
         }
+      },
     },
-    age : {
-        type : Number,
-        min : 18
+    age: {
+      type: Number,
+      min: 18,
     },
-    gender : {
-        type : String,
-        validate(value){
-            if(!["Male","Female","Other"].includes(value)){
-                throw new Error("Gender data is not valid");
-            }
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["Male", "Female", "Other"].includes(value)) {
+          throw new Error("Gender data is not valid");
         }
+      },
     },
-    password : {
-        type : String,
-        validate(value){
-            if(!validator.isStrongPassword(value)){
-                throw new Error("Please Enter a strong password");
-            }
+    password: {
+      type: String,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Please Enter a strong password");
         }
+      },
     },
-    photoUrl : {
-       type : String,
-       default : "https://cdn-icons-png.flaticon.com/512/9187/9187604.png",
-       validate(value){
-        if(!validator.isURL(value)){
-            throw new Error("URL is not valid : "+ value);
+    photoUrl: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/9187/9187604.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("URL is not valid : " + value);
         }
-       } 
+      },
     },
-    about : {
-        type : String,
-        default : "I am a developer!!"
+    about: {
+      type: String,
+      default: "I am a developer!!",
     },
-    skills : {
-        type : [String]
-    }
-},
-{
-    timestamps : true
-})
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
+userSchema.methods.getJWT = async function () {
+  const user = this;
+
+  const token = await jwt.sign({ _id: user._id }, "Dev@Tinder2024", {
+    expiresIn: "1d",
+  });
+
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordGivenByUser) {
+  const user = this;
+  const passwordHash = user.password;
+
+  const isPasswordValid = await bcrypt.compare(
+    passwordGivenByUser,
+    passwordHash
+  );
+
+  return isPasswordValid;
+};
 //Creating & Exporting the user model.
 module.exports = mongoose.model("User", userSchema);
