0592de602197a0b5f5deb353084c5d228d67ceb2
Parasharam Pujari <pujarip516@gmail.com>
2025-03-08 11:15:48 +0530

    updated password in database file

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://pujarip516:wvt8vbgXZJ12GRyt@cluster0.mzecj.mongodb.net/devTinder"
-    );
-}
+  await mongoose.connect(
+    "mongodb+srv://pujarip516:cc8TayAVBWaayDyU@cluster0.mzecj.mongodb.net/devTinder"
+  );
+};
 
 module.exports = connectDB;
-
